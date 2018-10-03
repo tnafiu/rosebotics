fd25df1824fda3a18d44eb1ee45e2a089b6e3788
thuerje <thuerje@rose-hulman.edu>
2018-10-03 08:36:06 -0400

    added ".value" to the end of the stop function

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -24,7 +24,7 @@ class Snatch3rRobot(object):
         self.left_wheel.start_spinning(left_wheel_duty_cycle_percent)
         self.right_wheel.start_spinning(right_wheel_duty_cycle_percent)
 
-    def stop(self, stop_action=StopAction.BRAKE):
+    def stop(self, stop_action=StopAction.BRAKE.value):
         self.left_wheel.stop_spinning(stop_action)
         self.right_wheel.stop_spinning(stop_action)
 
